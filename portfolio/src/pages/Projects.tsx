63244b600c854167483463556618ac6d76940363
romanie delporte <romaniedelporte230@gmail.com>
2024-05-10 14:42:25 +0200

    update

--- portfolio/src/pages/Projects.tsx
+++ portfolio/src/pages/Projects.tsx
@@ -10,7 +10,7 @@ export default function Projects() {
                 <div className="flex flex-col mt-40 sm:mt-20">
                     <h2 className="text-rose-500 text-2xl shadow-rose-300 font-medium text-center">My Projects</h2>
                     <div className="grid grid-cols-1 gap-12 mt-4 md:grid-cols-2 md:gap-8 lg:grid-cols-3 xl:grid-cols-3 xl:gap-16">
-                        {cardData.map((card) => (
+                        {cardData.map(card => (
                             <Card
                                 key={card.title}
                                 title={card.title}
